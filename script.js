3fc66995201f9ee86184be0848c0ac31d1d0e119
Crippao <cristina.paoloni@outlook.com>
2023-03-30 21:13:32 +0200

    aggiunta funzioni astratte

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-async function OnClick() {
-	const div = document.getElementById("span");
-	div.innerHTML = "";
+"use strict";
 
+function getNumberChoosenByUser() {
 	const num = document.getElementById("num");
 	const n = Number.parseInt(num.value);
+	return n;
+}
+
+function delay() {
+	return new Promise((res) => setTimeout(res, 20));
+}
+
+function getContainer() {
+	const c = document.getElementById("numbers-container");
+	return c;
+}
+
+function removeAllNumbersDiv() {
+	const c = getContainer();
+	c.innerHTML = "";
+}
+
+function getDefaultDiv(id) {
+	const elemento = document.querySelector(`[data-id="${id}"]`);
+	return elemento;
+}
+
+function shouldHideDiv() {
+	const check = document.getElementById("onlyPrimi");
+	const c = check.checked;
+	return c;
+}
+
+async function onClick() {
+	removeAllNumbersDiv();
+	const container = getContainer();
+
+	const n = getNumberChoosenByUser();
+
 	if (isNaN(n)) {
 		alert("Inserisci un valore");
 		return;
@@ -11,9 +44,7 @@ async function OnClick() {
 
 	const array = [];
 
-	const elemento = document.getElementById("span");
-
-	for (i = 0; i <= n; i++) {
+	for (let i = 0; i <= n; i++) {
 		array.push(i);
 		const div = document.createElement("div");
 
@@ -21,17 +52,16 @@ async function OnClick() {
 		div.classList.add("box");
 		div.innerText = array[i];
 
-		elemento.appendChild(div);
+		container.appendChild(div);
 	}
 
-	const check = document.getElementById("onlyPrimi");
-	const c = check.checked;
+	const c = shouldHideDiv();
 
 	// è stata inserita la radice quadrata perchè è necessario controllare i multipli solo fino
 	// alla metà del numero stesso
-	for (i = 2; i < Math.sqrt(n); i++) {
+	for (let i = 2; i < Math.sqrt(n); i++) {
 		if (array[i] === 0) continue;
-		for (j = i + 1; j <= n; j++) {
+		for (let j = i + 1; j <= n; j++) {
 			const elementoCorrente = getDefaultDiv(j);
 			if (array[j] % array[i] === 0) {
 				if (c) {
@@ -52,18 +82,8 @@ async function OnClick() {
 	alert("Calcolo Completato");
 }
 
-function delay() {
-	return new Promise((res) => setTimeout(res, 20));
-}
-
-function reset() {
-	const div = document.getElementById("span");
-	div.innerHTML = "";
+function Reset() {
+	removeAllNumbersDiv();
 	const text = document.getElementById("num");
 	text.value = null;
-}
-
-function getDefaultDiv(id) {
-	const elemento = document.querySelector(`[data-id="${id}"]`);
-	return elemento;
 }
