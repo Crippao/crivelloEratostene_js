a29e8f898172ddf57d5adfbf8445ae9a3993fadf
Crippao <cristina.paoloni@outlook.com>
2023-03-30 19:38:42 +0200

    Add Bootstrap

---
+++ crivelloEratostene_js-main/README.md
@@ -0,0 +1,1 @@
+# crivelloEratostene_js
---
+++ crivelloEratostene_js-main/script.js
@@ -0,0 +1,58 @@
+async function OnClick() {
+	const div = document.getElementById("span");
+	div.innerHTML = "";
+
+	const num = document.getElementById("num");
+	const n = num.value;
+
+	const array = [];
+
+	for (i = 0; i <= n; i++) {
+		array.push(i);
+		const elemento = document.getElementById("span");
+		const div = document.createElement("div");
+		div.setAttribute("id", `div${i}`);
+		div.setAttribute("data-id", i);
+		div.classList.add("box", "boxNoPrimi");
+		div.innerText = array[i];
+		elemento.appendChild(div);
+	}
+
+	const check = document.getElementById("onlyPrimi");
+	const c = check.checked;
+
+	for (i = 2; i < Math.sqrt(n); i++) {
+		if (array[i] === 0) continue;
+		for (j = i + 1; j <= n; j++) {
+			const elemento = document.getElementById(`div${j}`);
+			if (array[j] % array[i] === 0) {
+				if (c) {
+					const elem = document.getElementById("div0");
+					elem.style.display = "none";
+					elemento.style.display = "none";
+				}
+				array[j] = 0;
+				elemento.innerText = j;
+				elemento.className = "box boxNoPrimi";
+			} else {
+				elemento.className = "box boxPrimi";
+			}
+			await Delay();
+		}
+	}
+	const primi = array.filter((e) => {
+		return e !== 0;
+	});
+	alert("Calcolo Completato");
+}
+
+function Delay() {
+	return new Promise((res) => setTimeout(res, 10));
+}
+
+function Reset() {
+	const div = document.getElementById("span");
+	div.innerHTML = "";
+	const text = document.getElementById("num");
+	text.value = null;
+}
